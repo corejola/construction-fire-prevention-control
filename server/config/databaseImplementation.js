926a1b3fac3d6dcfaf4ca0b2ff49a43e0fa152f8
Benk0033 <benk0033@gmail.com>
2019-08-19 22:59:20 -0700

    saving

--- server/config/databaseImplementation.js
+++ server/config/databaseImplementation.js
@@ -1,6 +1,6 @@
-const mongoose 			 = require('mongoose');
+const mongoose = require('mongoose');
 const configDB = require('./databaseCreds');
-mongoose.connect(configDB.url);
+mongoose.connect(configDB.url, { useNewUrlParser: true });
 
 //Get the default connection
 const db = mongoose.connection;
@@ -8,6 +8,6 @@ const db = mongoose.connection;
 //Bind connection to error event (to get notification of connection errors)
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-db.once("open", function() {
+db.once("open", function () {
   console.log("Mongoose connection successful.");
 });
