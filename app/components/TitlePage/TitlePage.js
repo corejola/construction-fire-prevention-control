e45f2129301a095759b50c9835f572ed09f2aad5
LORRAINE BAYNOSA <you@example.com>
2019-08-21 21:06:44 -0700

    working route with click

--- app/components/TitlePage/TitlePage.js
+++ app/components/TitlePage/TitlePage.js
@@ -1,40 +1,47 @@
-import React, {Component} from 'react';
-import {Button, Colors, ButtonGroup, Link, Breakpoints} from 'react-foundation';
+import React, { Component } from 'react';
+// import { Button, Colors, ButtonGroup, Link, Breakpoints } from 'react-foundation';
+import { Colors, Button } from 'react-foundation';
+import { Link } from "react-router-dom";
+
 // import './style.css';
 
-class TitlePage extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-        isClicked: false
-    }
-    }
-
-    clickHandle() {
-        console.log(`submit clicked`)
-        this.setState(prevState => (
-            {isClicked: !prevState.isClicked}
-            ))
-        alert("You are being redirected")
-    }
-
-    render() {
-
+function TitlePage (props) {
         return (
             // <div className="container">
             // <div className="button-group-stack-example">
             // <ButtonGroup stackFor={Breakpoints.SMALL}>
 
             <div className="container">
-            <div className="button-group">  
-            <ButtonGroup isExpanded> 
-            
-            <Link 
+                {/* <div className="button-group"> */}
+                    {/* <ButtonGroup isExpanded> */}
+                        {/* <Link
+                            onClick={(e) => { e.preventDefault(); this.clickHandle(); }}
+                            color={Colors.SUCCESS}
+                            to="/assessment"
+                        >
+                            TOWER 1
+                        </Link> */}
+
+                        {/* WORKING LINK */}
+                        <div className="button-basics-example">
+                            <Button color={Colors.SUCCESS}>TOWER1</Button>
+                            <Link
+                            // onClick={this.clickHandle()}
+                            color={Colors.SUCCESS}
+                            to="/assessment" 
+                            role="button"
+                            className="btn btn-link"
+                        >TOWER 1
+                        </Link>
+    
+                        
+                        
+                        {/* <Link 
             onClick={(e) => {e.preventDefault(); this.clickHandle();}} color={Colors.SUCCESS}><a className="userguide" href="/userguide">USERGUIDE</a>
             </Link>
-
+            
             <Link
-            onClick={(e) => {e.preventDefault(); this.clickHandle();}} color={Colors.SUCCESS}><a className="tower1" href="/tower1">TOWER 1</a>
+            onClick={(e) => {e.preventDefault(); this.clickHandle();}} color={Colors.SUCCESS}><a className="tower1" href="/assessment">TOWER 1</a>
             </Link>
 
             <Link
@@ -48,22 +55,23 @@ class TitlePage extends Component {
             <Link
             onClick={(e) => {e.preventDefault(); this.clickHandle();}} color={Colors.SUCCESS}><a className="podium" href="#">RETAIL PODIUM (TBD)</a>
             </Link>
-
+            
             <Link
-            onClick={(e) => {e.preventDefault(); this.clickHandle();}} color={Colors.SUCCESS}><a className="assessments" href="/assessment">CURRENT FIRE RISK ASSESSMENT</a>
+            onClick={(e) => {e.preventDefault(); this.clickHandle();}} color={Colors.SUCCESS}><a className="assessments" href="/#">CURRENT FIRE RISK ASSESSMENT</a>
             </Link>
           
             <Link
-            onClick={(e) => {e.preventDefault(); this.clickHandle();}} color={Colors.SUCCESS}><a className="trending" href="#">FIRE RISK ASSESSMENT TRENDING</a>
-            </Link>
 
-            </ButtonGroup>
-            </div>
-              <h1>Hello</h1>  
+            onClick={(e) => {e.preventDefault(); this.clickHandle();}} color={Colors.SUCCESS}><a className="trending" href="#">FIRE RISK ASSESSMENT TRENDING</a>
+            </Link> */}
+
+                    {/* </ButtonGroup> */}
+                </div>
+                <h1>Hello</h1>
             </div>
         )
     }
-}
+
 
 export default TitlePage;
 
