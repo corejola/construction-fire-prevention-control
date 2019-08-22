85fae80aca389edcd11d9afe41ab6fc51d7fbc99
LORRAINE BAYNOSA <you@example.com>
2019-08-22 07:32:31 -0700

    merging updated master branch with my branch

--- app/components/TitlePage/TitlePage.js
+++ app/components/TitlePage/TitlePage.js
@@ -1,77 +1,41 @@
 import React, { Component } from 'react';
-// import { Button, Colors, ButtonGroup, Link, Breakpoints } from 'react-foundation';
-import { Colors, Button } from 'react-foundation';
-import { Link } from "react-router-dom";
+import { Button, ButtonGroup, Link as Lin, Breakpoints, Colors } from 'react-foundation';
+import { Link} from 'react-router-dom'; 
+
+
 
 // import './style.css';
 
 function TitlePage (props) {
         return (
-            // <div className="container">
-            // <div className="button-group-stack-example">
-            // <ButtonGroup stackFor={Breakpoints.SMALL}>
-
             <div className="container">
-                {/* <div className="button-group"> */}
-                    {/* <ButtonGroup isExpanded> */}
-                        {/* <Link
-                            onClick={(e) => { e.preventDefault(); this.clickHandle(); }}
+                <div className="button-group-stack-example">
+  <ButtonGroup stackFor={Breakpoints.SMALL}>
+    <Lin color={Colors.SUCCESS}>How</Lin>
+    {/* <Lin><Link to="/assessment"></Link>Low</Lin> */}
+    <Lin class="is-active">Can</Lin>
+    <Lin>You</Lin>
+    <Lin>Go</Lin>
+  </ButtonGroup>
+                </div>
+
+                {/* <div className="button-basics">
+                    <Button>
+                        <Link
                             color={Colors.SUCCESS}
                             to="/assessment"
-                        >
-                            TOWER 1
-                        </Link> */}
-
-                        {/* WORKING LINK */}
-                        <div className="button-basics-example">
-                            <Button color={Colors.SUCCESS}>TOWER1</Button>
-                            <Link
-                            // onClick={this.clickHandle()}
-                            color={Colors.SUCCESS}
-                            to="/assessment" 
-                            role="button"
                             className="btn btn-link"
-                        >TOWER 1
+                            role="button">
+                            TOWER 1
                         </Link>
-    
-                        
-                        
-                        {/* <Link 
-            onClick={(e) => {e.preventDefault(); this.clickHandle();}} color={Colors.SUCCESS}><a className="userguide" href="/userguide">USERGUIDE</a>
-            </Link>
-            
-            <Link
-            onClick={(e) => {e.preventDefault(); this.clickHandle();}} color={Colors.SUCCESS}><a className="tower1" href="/assessment">TOWER 1</a>
-            </Link>
-
-            <Link
-            onClick={(e) => {e.preventDefault(); this.clickHandle();}} color={Colors.SUCCESS}><a className="tower2" href="#">TOWER 2 (TBD)</a>
-            </Link>
-
-            <Link
-            onClick={(e) => {e.preventDefault(); this.clickHandle();}} color={Colors.SUCCESS}><a className="tower3" href="#">TOWER 3 (TBD)</a>
-            </Link>
-
-            <Link
-            onClick={(e) => {e.preventDefault(); this.clickHandle();}} color={Colors.SUCCESS}><a className="podium" href="#">RETAIL PODIUM (TBD)</a>
-            </Link>
-            
-            <Link
-            onClick={(e) => {e.preventDefault(); this.clickHandle();}} color={Colors.SUCCESS}><a className="assessments" href="/#">CURRENT FIRE RISK ASSESSMENT</a>
-            </Link>
-          
-            <Link
-
-            onClick={(e) => {e.preventDefault(); this.clickHandle();}} color={Colors.SUCCESS}><a className="trending" href="#">FIRE RISK ASSESSMENT TRENDING</a>
-            </Link> */}
-
-                    {/* </ButtonGroup> */}
-                </div>
+                    </Button> 
                 <h1>Hello</h1>
+                </div> */}
             </div>
         )
     }
 
 
 export default TitlePage;
+
 
