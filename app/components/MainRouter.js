853707b5d6d88562bb2370ba3660ef6dbe88980b
Benk0033 <benk0033@gmail.com>
2019-08-22 20:25:20 -0700

    added auth to new routes

--- app/components/MainRouter.js
+++ app/components/MainRouter.js
@@ -7,7 +7,6 @@ import {
 import Main from "./Main";
 import Login from "./Login";
 import Signup from "./Signup";
-import Pricing from "./Pricing";
 import axios from 'axios';
 import Assessment from "./Assessment/index";
 import TitlePage from './TitlePage/TitlePage';
@@ -67,7 +66,7 @@ export default class MainRouter extends Component {
           <PrivateRoute
             exact
             path="/"
-            component={Main}
+            component={TitlePage}
             authenticate={this.authenticate}
             deAuthenticate={this.deAuthenticate}
             authenticated={this.state.authenticated}
@@ -92,22 +91,13 @@ export default class MainRouter extends Component {
               logout={this.logout}
             />}
           />
-          <Route strict exact path="/titlepage" component={TitlePage}
-          />
+          {/* <Route strict exact path="/titlepage" component={TitlePage}
+          /> */}
           <Route strict exact path="/userguide" component={UserGuide}
           />
           <Route strict exact path="/weather" component={WeatherContainer}
           />
           <Route strict exact path="/weatherform" component={WeatherForm}
-          />
-          <Route strict exact path="/pricing" render={props =>
-            <Pricing
-              {...props}
-              authenticate={this.authenticate}
-              deAuthenticate={this.deAuthenticate}
-              authenticated={this.state.authenticated}
-              logout={this.logout}
-            />}
           />
           <Route strict exact path="/assessment" component={Assessment} />
         </Switch>
