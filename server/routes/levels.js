2c06b502e9e95eef6569f97c9602acf233c29ab8
Benk0033 <benk0033@gmail.com>
2019-09-09 20:56:13 -0700

    added back auth

--- server/routes/levels.js
+++ server/routes/levels.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const level_api = require('../apis/level_api');
 const authCheck = require("../config/middleware/authCheck");
 
-// router.get('/:id', authCheck, level_api.findAllLevels);
-router.get('/:id', level_api.findAllLevels);
+router.get('/:id', authCheck, level_api.findAllLevels);
+// router.get('/:id', level_api.findAllLevels);
 
 module.exports = router;
