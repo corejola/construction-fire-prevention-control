e734ad88ee9995ecbccecc311cf55fd93f7467e8
Cedric Orejola <cedricorejola@gmail.com>
2019-09-11 20:37:27 -0700

    deploy for demo day part 2

--- webpack.config.js
+++ webpack.config.js
@@ -33,7 +33,6 @@ module.exports = {
   // This lets us debug our react code in chrome dev tools. Errors will have lines and file names
   // Without this the console says all errors are coming from just coming from bundle.js
   devtool: "eval-source-map",
-  mode: 'development',
-  watch: true
+  mode: 'development'
 };
 
