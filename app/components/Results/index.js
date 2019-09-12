556aa580fa6cd1c64f6135e0cb0c356bc03e20cc
nance <48306129+NanceCA@users.noreply.github.com>
2019-09-11 19:03:03 -0700

    finalizing style pages for the results page

--- app/components/Results/index.js
+++ app/components/Results/index.js
@@ -7,13 +7,17 @@ import { Button } from 'react-foundation'
 
 // //---------- BAR CHART VARIABLES ----------//
 const bw = 400;
-const bh = 570;
+const bh = 600;
 const pw = 400;
 const ph = 400;
 const margin = 40;
 const radius = Math.min(pw, ph) / 2 - margin;
 
 const styles = {
+    title: {
+        "textAlign": "left",
+        "padding": "15px 0 0 100px"
+    },
     holder: {
         "display": "flex",
         "flexDirection": "column",
@@ -40,16 +44,32 @@ const styles = {
         "display": "flex",
         "flexDirection": "row",
         "alignItems": "center",
-        "justifyContent": "space-around"
+        "justifyContent": "space-around",
+        "padding": "0 200px 0 200px"
     },
-}
+    bar: {
+        "backgroundColor": "black",
+        "height": "30px",
+        "padding": "15px 40px 15px 40px",
+        "margin": "15px 40px 15px 40px"
+    },
+    hr: {
+        "display": "block",
+        "marginTop": "0.5em",
+        "marginBottom": "0.5em",
+        "marginLeft": "auto",
+        "marginRight": "auto",
+        "borderStyle": "inset",
+        "borderWidth": "1px"
+    }
+};
 
 const small = {
     "display": "flex",
     "flexDirection": "column",
     "alignItems": "center",
     "justifyContent": "space-around",
-    "padding": "0 150px 0 150px"
+    "padding": "0 300px 0 300px"
 };
 
 
@@ -137,11 +157,10 @@ class Results extends React.Component {
                     .data(this.state.dataset)
                     .enter()
                     .append("rect")
-                    .attr("x", 100)
+                    .attr("x", 160)
                     .attr("y", (d, i) => bh - ((i + 1) * 10))
-                    // .attr("y", (d, i) => bh - ((i + 1) * 30))
                     .attr("class", "barG")
-                    .attr("width", 200)
+                    .attr("width", 150)
                     .attr("height", 7)
                     .attr("fill", (d) => {
                         if (d === "normal") {
@@ -247,26 +266,26 @@ class Results extends React.Component {
 
     render() {
 
-        // // const redFloors = database.filter(user => user.online); // change code here
-        // const redFloors = database.map((i) => <li key={i.username + 1}>{i.username}</li>); // change code here
-
         if (this.state.display) {
             return (
                 <div>
+                    <div style={styles.bar}></div>
+
+                    <h3 style={styles.title}> I.Tower Readiness Results</h3>
+                    <hr />
                     <div style={styles.holder}>
-                        <h1>Tower Readiness Results</h1>
 
                         <p className="description"><strong>ABOUT THE VISUALIZATIONS: </strong>The following visualization aggregates the tower's evaluation data. After evaluating the current state of each floor, calculations are conducted on the back-end to determine if a floor is in a good or normal safety status, <strong>green</strong>, a caution safety status, <strong>yellow</strong>,or a critical  status, <strong>red</strong>. Hover over each floor to see the final score. Notice if certain floors continue to evaluate to a severe safety rating. <br /> <br />              Reference the pie chart below to see the percentage of floors that are in each category: normal, caution, critical.</p>
                         <Button onClick={this.handleClick}>More Details</Button>
-
                     </div >
 
+
                     <div className="vis-holder" style={this.state.width < 1000 ? small : styles.visHolder}>
+
                         <h4>TOWER 1 RESULTS</h4>
                         <h4>PERCENTAGE OF RESULTS</h4>
                     </div>
 
-                    {/* <div className="data-labels" style={styles.data}> */}
                     <div className="data-labels" style={this.state.width < 1000 ? styles.holder : styles.data}>
 
                         <p id="green">{`NORMAL: ${parseInt(this.state.green / this.state.dataset.length * 100)} %`}</p>
@@ -281,19 +300,30 @@ class Results extends React.Component {
                         <div className="pie-chart"></div>
 
                     </div>
+                    <h3 style={styles.title}> II. About the Methodology</h3>
+                    <hr />
+                    <div style={styles.holder}>
+
+                        <p className="description">Each floor has a corresponding safety survey that results in a health rating: normal, caution or in a critical condition. Each floor depicted in the tower is a bar chart that calculates the aggregate rating for each floor, given the ratings that it received that day. The pie chart calculates how each category contributes to the tower status as a whole. Click on the more details button to view how many floors are tagged to be in critical condition and may need attention to resume to a normal safety status.     </p>
+                    </div >
+                    <div style={styles.bar}></div>
                 </div >
             );
         }
         else {
             return (
                 <div>
-                    <div style={styles.holder}>
-                        <h1>Tower Readiness Results</h1>
+                    <div style={styles.bar}></div>
+                    <h3 style={styles.title}> I.Tower Readiness Results</h3>
+                    <hr />
 
-                        <p className="description"><strong>FLOORS IN CRITICAL CONDITION:</strong><br /><br />
-                            {`Total Floors:  ${this.state.red}`}
+                    <div style={styles.holder}>
+
+                        <p className="description"><strong>FLOORS IN CRITICAL CONDITION:</strong>{`  ${this.state.red}`}
+                            <br /> <br />
+                            Contact area superintendent for support.
                         </p>
-                        <button onClick={this.handleClick}>Show Graph Descriptions</button>
+                        <Button onClick={this.handleClick}>Show Graph Descriptions</Button>
 
                     </div >
 
@@ -317,6 +347,14 @@ class Results extends React.Component {
                         <div className="pie-chart"></div>
 
                     </div>
+
+                    <h3 style={styles.title}> II. About the Methodology</h3>
+                    <hr />
+                    <div style={styles.holder}>
+
+                        <p className="description">Each floor has a corresponding safety survey that results in a health rating: normal, caution or in a critical condition. Each floor depicted in the tower is a bar chart that calculates the aggregate rating for each floor, given the ratings that it received that day. The pie chart calculates how each category contributes to the tower status as a whole. Click on the more details button to view how many floors are tagged to be in critical condition and may need attention to resume to a normal safety status.     </p>
+                    </div >
+                    <div style={styles.bar}></div>
 
                 </div >
 
--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -9,168 +9,168 @@ mongoose.connect(
     "mongodb://127.0.0.1/fire-risk-assessment-db"
 );
 
-// const userSeed = [
-//     {
-//         username: "testman",
-//         email: "test@test.com",
-//         password: "Password123!"
-//     },
-//     {
-//         username: "blahman",
-//         email: "blah@blah.com",
-//         password: "Password123!"
-//     }
-// ];
+const userSeed = [
+    {
+        username: "testman",
+        email: "test@test.com",
+        password: "Password123!"
+    },
+    {
+        username: "blahman",
+        email: "blah@blah.com",
+        password: "Password123!"
+    }
+];
 
-// db.User
-//     .remove({})
-//     .then(() => db.User.collection.insertMany(userSeed))
-//     .then(data => {
+db.User
+    .remove({})
+    .then(() => db.User.collection.insertMany(userSeed))
+    .then(data => {
 
-db.User.find({}).then((users) => {
+        db.User.find({}).then((users) => {
 
-    const surveySeed = [
-        {
-            extinguishers: false,
-            smokeFireDetection: false,
-            smokeFireAlarms: false,
-            fireRisers: false,
-            emergencyEgress: false,
-            riskAssessmentResult: "critical",
-            assessementDate: new Date(Date.now()),
-            user: users[0]._id
-        },
-        {
-            extinguishers: true,
-            smokeFireDetection: true,
-            smokeFireAlarms: true,
-            fireRisers: true,
-            emergencyEgress: true,
-            riskAssessmentResult: "normal",
-            assessementDate: new Date(Date.now()),
-            user: users[1]._id
-        }
-    ];
+            const surveySeed = [
+                {
+                    extinguishers: false,
+                    smokeFireDetection: false,
+                    smokeFireAlarms: false,
+                    fireRisers: false,
+                    emergencyEgress: false,
+                    riskAssessmentResult: "critical",
+                    assessementDate: new Date(Date.now()),
+                    user: users[0]._id
+                },
+                {
+                    extinguishers: true,
+                    smokeFireDetection: true,
+                    smokeFireAlarms: true,
+                    fireRisers: true,
+                    emergencyEgress: true,
+                    riskAssessmentResult: "normal",
+                    assessementDate: new Date(Date.now()),
+                    user: users[1]._id
+                }
+            ];
 
-    db.SurveyRiskAssessment
-        .remove({})
-        .then(() => db.SurveyRiskAssessment.collection.insertMany(surveySeed))
-        .then(data => {
+            db.SurveyRiskAssessment
+                .remove({})
+                .then(() => db.SurveyRiskAssessment.collection.insertMany(surveySeed))
+                .then(data => {
 
-            db.SurveyRiskAssessment.find({}).then((surveys) => {
+                    db.SurveyRiskAssessment.find({}).then((surveys) => {
 
-                const levelSeed = [
-                    {
-                        levelNumber: "B2",
-                        riskAssessments: []
-                    },
-                    {
-                        levelNumber: "B1",
-                        riskAssessments: []
-                    },
-                    {
-                        levelNumber: "Ground",
-                        riskAssessments: []
-                    },
-                    {
-                        levelNumber: "22/Mechanical",
-                        riskAssessments: []
-                    },
-                    {
-                        levelNumber: "PH1",
-                        riskAssessments: []
-                    },
-                    {
-                        levelNumber: "PH2",
-                        riskAssessments: []
-                    },
-                    {
-                        levelNumber: "PH3",
-                        riskAssessments: []
-                    },
-                    {
-                        levelNumber: "PH4",
-                        riskAssessments: []
-                    },
-                    {
-                        levelNumber: "ROOF",
-                        riskAssessments: []
-                    },
-                    {
-                        levelNumber: "MECH. ELEV. MACH.",
-                        riskAssessments: []
-                    },
-                    {
-                        levelNumber: "MECH. ROOF",
-                        riskAssessments: []
-                    }
-                ];
+                        const levelSeed = [
+                            {
+                                levelNumber: "B2",
+                                riskAssessments: []
+                            },
+                            {
+                                levelNumber: "B1",
+                                riskAssessments: []
+                            },
+                            {
+                                levelNumber: "Ground",
+                                riskAssessments: []
+                            },
+                            {
+                                levelNumber: "22/Mechanical",
+                                riskAssessments: []
+                            },
+                            {
+                                levelNumber: "PH1",
+                                riskAssessments: []
+                            },
+                            {
+                                levelNumber: "PH2",
+                                riskAssessments: []
+                            },
+                            {
+                                levelNumber: "PH3",
+                                riskAssessments: []
+                            },
+                            {
+                                levelNumber: "PH4",
+                                riskAssessments: []
+                            },
+                            {
+                                levelNumber: "ROOF",
+                                riskAssessments: []
+                            },
+                            {
+                                levelNumber: "MECH. ELEV. MACH.",
+                                riskAssessments: []
+                            },
+                            {
+                                levelNumber: "MECH. ROOF",
+                                riskAssessments: []
+                            }
+                        ];
 
-                for (let i = 21; i > 1; i--) {
+                        for (let i = 21; i > 1; i--) {
 
-                    levelSeed.splice(3, 0, {
-                        levelNumber: "" + i + "",
-                        riskAssessments: []
-                    })
+                            levelSeed.splice(3, 0, {
+                                levelNumber: "" + i + "",
+                                riskAssessments: []
+                            })
 
-                };
+                        };
 
-                for (let i = 50; i > 22; i--) {
+                        for (let i = 50; i > 22; i--) {
 
-                    levelSeed.splice(24, 0, {
-                        levelNumber: "" + i + "",
-                        riskAssessments: []
-                    })
+                            levelSeed.splice(24, 0, {
+                                levelNumber: "" + i + "",
+                                riskAssessments: []
+                            })
 
-                };
+                        };
 
-                db.Level
-                    .remove({})
-                    .then(() => db.Level.collection.insertMany(levelSeed))
-                    .then(data => {
+                        db.Level
+                            .remove({})
+                            .then(() => db.Level.collection.insertMany(levelSeed))
+                            .then(data => {
 
-                        db.Level.find({}).then((levels) => {
+                                db.Level.find({}).then((levels) => {
 
-                            const towerSeed = [
-                                {
-                                    towerNumber: "1",
-                                    levels: []
-                                }
-                            ];
+                                    const towerSeed = [
+                                        {
+                                            towerNumber: "1",
+                                            levels: []
+                                        }
+                                    ];
 
-                            for (let i = 0; i < levels.length; i++) {
-                                towerSeed[0].levels.push(levels[i]._id)
-                            };
+                                    for (let i = 0; i < levels.length; i++) {
+                                        towerSeed[0].levels.push(levels[i]._id)
+                                    };
 
-                            db.Tower
-                                .remove({})
-                                .then(() => db.Tower.collection.insertMany(towerSeed))
-                                .then(data => {
-                                    console.log(data.result.n + " records inserted!");
-                                    process.exit(0);
-                                })
-                                .catch(err => {
-                                    console.error(err);
-                                    process.exit(1);
+                                    db.Tower
+                                        .remove({})
+                                        .then(() => db.Tower.collection.insertMany(towerSeed))
+                                        .then(data => {
+                                            console.log(data.result.n + " records inserted!");
+                                            process.exit(0);
+                                        })
+                                        .catch(err => {
+                                            console.error(err);
+                                            process.exit(1);
+                                        });
+
                                 });
 
-                        });
+                            })
+                            .catch(err => {
+                                console.error(err);
+                                process.exit(1);
+                            });
 
-                    })
-                    .catch(err => {
-                        console.error(err);
-                        process.exit(1);
                     });
 
-            });
+                })
+                .catch(err => {
+                    console.error(err);
+                    process.exit(1);
+                });
 
-        })
-        .catch(err => {
-            console.error(err);
-            process.exit(1);
         });
 
-});
 
-
-
+    });
